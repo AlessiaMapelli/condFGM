52214bcd5167c077e9af98eb5cc4b04807269a08
LauraCarini <laura.carini@edu.unige.it>
2026-05-28 10:50:02 +0200

    Upload updated codes

---
+++ EEG_data_application/Results_evaluation_matrix_plot.R
@@ -0,0 +1,242 @@
+rm(list=ls(all=TRUE))
+packages <- c('yaml')
+install.packages(setdiff(packages, rownames(installed.packages())), dependencies = TRUE)
+suppressPackageStartupMessages(library(yaml))
+suppressPackageStartupMessages(library(reshape2))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(dplyr))
+
+#################################################
+## USER DEFINED PARAMETERS (MODIFY THE PATH TO THE CORRECT YAML FILE)
+#################################################
+
+args <- commandArgs(trailingOnly = TRUE)
+yaml_file_path = "config_file.yaml"
+config <- yaml.load_file(yaml_file_path)
+
+
+#########
+path_sensors =  "Position_list.Rdata"
+load(path_sensors)  # load pos.list
+p <- 64
+node.names <- numeric(p)
+for (i in 1:p){
+  node.names[i] <- pos.list[[i]]
+}
+#######
+
+#################################################
+## 0. USER DEFINED PARAMETERS (MODIFY THIS PART)
+#################################################
+output_path = paste("",config$output_path, sep ="")
+name_output = config$name_output
+input_path =  paste("", config$input_path, sep ="")
+type = config$type
+
+load(input_path)
+
+if ((exists("covariates_df", envir = .GlobalEnv) && is.data.frame(covariates_df))){
+  C <- model.matrix(~ ., covariates_df)
+  n_groups <- ncol(C)
+  cov_names <- colnames(C)
+  cov_names[1] <- "population"
+} else{ n_groups <- 1
+cov_names <- "population" }
+
+num_nodes = config$n_nodes
+n_comp <- config$n_basis_for_dim_recustion
+
+##############################################
+
+G.our <- matrix(NA, nrow = num_nodes, ncol = num_nodes*n_groups)
+for(j in 1: num_nodes){
+  load(paste(output_path, name_output,"_" ,j, ".rda", sep=""))
+  G.our[j, ] <- N.hat.optimal
+}
+
+G.our.symm = list()
+G.our.symm.weighted = list()
+G.our.symm.warning= list()
+G.our.weighted= list()
+
+
+for(i in 1:n_groups){
+  G.cov <- G.our[,(num_nodes*(i-1) +1):(num_nodes*i)]
+  G.cov.simm <- matrix(0, nrow = num_nodes, ncol = num_nodes)
+  if(type=="AND"){
+    for(j in 1:(num_nodes-1)){
+      for(k in (i+1):num_nodes){
+        if(G.cov[j,k] == 1 & G.cov[k,j] == 1){
+          G.cov.simm[j,k] = 1
+          G.cov.simm[k,j] = 1
+        }
+      }
+    }
+  }else if(type=="OR"){
+    for(j in 1:(num_nodes-1)){
+      for(k in (i+1):num_nodes){
+        if(G.cov[j,k] == 1 | G.cov[k,j] == 1){
+          G.cov.simm[j,k] = 1
+          G.cov.simm[k,j] = 1
+        }
+      }
+    }
+  }else{
+    G.cov.simm = G.cov
+  }
+  
+  if(i==1){
+    plot_title <- paste0("Node adjacency Matrix - ", cov_names[i])
+  } else {
+    plot_title <- paste0("Node adjacency Matrix - Differential contribution ", cov_names[i])
+  }
+  
+  
+  G.our.symm[[cov_names[i]]] <- G.cov.simm
+  
+  adj_df <- melt(G.cov.simm, varnames = c("Row","Col"), value.name = "Value")
+  adj_df <- adj_df %>%
+    mutate(
+      Row = factor(as.numeric(as.character(Row)), levels = 1:64, labels = node.names),
+      Col = factor(as.numeric(as.character(Col)), levels = 1:64, labels = node.names)
+    )
+  
+  adj_df <- adj_df %>%
+    mutate(Row = factor(Row, levels = rev(node.names)))
+  
+  colnames(adj_df) <- c("Row", "Col", "Value")
+  adj_df$Value <- as.numeric(adj_df$Value)
+  plot <- ggplot(adj_df, aes(x = Col, y = Row, fill = Value)) +
+    geom_tile() +
+    scale_fill_gradient(low = "white", high = "black") +
+    theme_minimal() +
+    coord_fixed() +
+    # scale_y_reverse() +  # to match matrix view
+    # labs(title = plot_title, x = "", y = "")
+    theme(
+      axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5, hjust = 1),  # shrink x-axis labels and rotate
+      axis.text.y = element_text(size = 6)  # shrink y-axis labels
+    )
+  print(plot)
+  ggsave(plot, filename=paste0(output_path, name_output, "_Adjacency_matrix_node_",cov_names[i], ".png"), width=8, height=8, dpi=300)
+  
+  
+  if(i !=1){
+    G.cov.weighted <- matrix(NA, nrow = num_nodes, ncol = num_nodes)
+    for(j in 1: num_nodes){
+      for(k in 1: num_nodes){
+        if(G.cov[j,k]){
+          load(paste(output_path, name_output,"_" ,j, "coeff.rda", sep=""))
+          beta.g2.forbs <- norm(P.values[[k]] + P.values[[k+num_nodes]], "F")
+          if(P.frob[[k]] == 0){
+            cat("Warning: Zero norm at denominator. Setting to the numerator value")
+            G.cov.weighted[j,k] <- beta.g2.forbs
+          }else{
+            G.cov.weighted[j,k] <- beta.g2.forbs/(P.frob[[k]])
+          }
+        }
+      }
+    }
+    G.cov.simm.weighted <- matrix(NA, nrow = num_nodes, ncol = num_nodes)
+    G.cov.simm.warning <- matrix(NA, nrow = num_nodes, ncol = num_nodes)
+    for (j in 1:(num_nodes - 1)) {
+      for (k in (j + 1):num_nodes) {
+        if(G.cov.simm[j,k]){
+          load(paste(output_path, name_output,"_" ,j, "coeff.rda", sep=""))
+          beta.g2.forbs.jk <- norm(P.values[[k]] + P.values[[k+num_nodes]], "F")
+          if(P.frob[[k]] == 0){
+            cat("Warning: Zero norm at denominator. Setting to the numerator value")
+            ratio.beta.forbs.jk <- beta.g2.forbs.jk
+          }else{
+            ratio.beta.forbs.jk <- beta.g2.forbs.jk/(P.frob[[k]])
+          }
+          load(paste(output_path, name_output,"_" ,k, "coeff.rda", sep=""))
+          beta.g2.forbs.kj <- norm(P.values[[j]] + P.values[[j+num_nodes]], "F")
+          if(P.frob[[j]] == 0){
+            cat("Warning: Zero norm at denominator. Setting to the numerator value")
+            ratio.beta.forbs.kj <- beta.g2.forbs.kj
+          }else{
+            ratio.beta.forbs.kj <- beta.g2.forbs.kj/(P.frob[[j]])
+          }
+          if(G.cov[j,k] & G.cov[k,j]){
+            
+            if(ratio.beta.forbs.jk > 1 & ratio.beta.forbs.kj > 1){
+              G.cov.simm.warning[j,k] <- 0
+              G.cov.simm.warning[k,j] <- 0
+            } else if (ratio.beta.forbs.jk < 1 & ratio.beta.forbs.kj < 1){
+              G.cov.simm.warning[j,k] <- 0
+              G.cov.simm.warning[k,j] <- 0
+            } else{
+              G.cov.simm.warning[j,k] <- 1
+              G.cov.simm.warning[k,j] <- 1
+            }
+            x <- c(ratio.beta.forbs.jk, ratio.beta.forbs.kj)
+            G.cov.simm.weighted[j,k] <- exp(mean(log(x)))
+            G.cov.simm.weighted[k,j] <- exp(mean(log(x)))
+            
+          }else if (G.cov[j,k]){
+            G.cov.simm.warning[j,k] <- 0
+            G.cov.simm.warning[k,j] <- 0
+            G.cov.simm.weighted[j,k] <- ratio.beta.forbs.jk
+            G.cov.simm.weighted[k,j] <- ratio.beta.forbs.jk
+          }else{
+            G.cov.simm.warning[j,k] <- 0
+            G.cov.simm.warning[k,j] <- 0
+            G.cov.simm.weighted[j,k] <- ratio.beta.forbs.kj
+            G.cov.simm.weighted[k,j] <- ratio.beta.forbs.kj
+          }
+          
+        }
+      }
+    }
+    
+    G.our.symm.weighted[[cov_names[i]]] <- G.cov.simm.weighted
+    G.our.symm.warning[[cov_names[i]]] <- G.cov.simm.warning
+    G.our.weighted[[cov_names[i]]] <- G.cov.weighted
+    
+    adj_df <- melt(G.cov.simm.weighted, varnames = c("Row","Col"), value.name = "Value")
+    
+    adj_df <- adj_df %>%
+      mutate(
+        Row = factor(as.numeric(as.character(Row)), levels = 1:64, labels = node.names),
+        Col = factor(as.numeric(as.character(Col)), levels = 1:64, labels = node.names)
+      )
+    
+    adj_df <- adj_df %>%
+      mutate(Row = factor(Row, levels = rev(node.names)))
+    
+    
+    adj_df <- adj_df %>%
+      mutate(Value_logic = case_when(
+        is.na(Value)      ~ "0",
+        Value < 1         ~ "-2",
+        Value > 1         ~ "2",
+        TRUE              ~ "NA"
+      ))
+    
+    plot <- ggplot(adj_df, aes(x = Col, y = Row, fill = as.factor(Value_logic))) +
+      geom_tile() +
+      scale_fill_manual(
+        values = c("-2" = "blue", "1" = "black", "2" = "red", "0"="white"),
+        labels = c("-2" = "Reduced edges", "1" = "Common edges", "2" = "Enlarge edges", "0"="Edges not present"),
+        name = "Edge Type"
+      ) +
+      theme_minimal() +
+      coord_fixed() +
+      # scale_y_reverse() +
+      # labs(title = paste0("Full Estimated Adjacency Matrix - Differential contribution ", cov_names[i]), x = "", y = "") +
+      theme(
+        axis.text.x = element_text(size = 6, angle = 90, vjust = 0.5, hjust = 1),  # shrink x-axis labels and rotate
+        axis.text.y = element_text(size = 6)  # shrink y-axis labels
+      )
+    print(plot)
+  ggsave(plot, filename = paste0(output_path, name_output, "_Adjacency_matrix_node_diff_weights_",cov_names[i],".png"), width = 8, height = 8, dpi = 300)
+  }
+  
+}
+
+# Save the matrix
+save(G.our.symm,G.our.symm.weighted,G.our.symm.warning, G.our.weighted , file = paste0(output_path, name_output, "_Adj_estimation.rda"))
+cat("Results saved to ", paste(output_path, name_output, "_Adj_estimation.rda", sep=""), "\n")
+#################################################
+
---
+++ EEG_data_application/plot_topomap.R
@@ -0,0 +1,119 @@
+rm(list = ls())
+
+library("igraph")
+
+# Step 1. Load data
+folder = ""
+path_sensors = paste0(folder, "EEG_dataset\\", "Position_list.Rdata")
+path_adj = paste0(folder, "EEG_dataset\\seed_1\\results\\", "Test1_Adj_estimation.rda")
+
+load(path_sensors)  # load pos.list
+load(path_adj)  # load AdjMat
+
+# Step 2. Define sensor names and positions
+p <- 64
+node.names <- numeric(p)
+for (i in 1:p){
+  node.names[i] <- pos.list[[i]]
+}
+position.List <- list("FPZ"=c(0, 0.8), "AFZ"=c(0, 0.6), "FZ"=c(0, 0.4), FCZ=c(0, 0.2), 
+                      "CZ"=c(0, 0), "CPZ"=c(0, -0.2), "PZ"=c(0, -0.4), "POZ"=c(0, -0.6), 
+                      "OZ"=c(0, -0.8), "nd"=c(0,-1), "C2"=c(0.2, 0), "C4"=c(0.4, 0), 
+                      "C6"=c(0.6, 0), "T8"=c(0.8, 0), "Y"=c(1, 0), "C1"=c(-0.2, 0), 
+                      "C3"=c(-0.4, 0), "C5"=c(-0.6, 0), "T7"=c(-0.8, 0), "X"=c(-1, 0), 
+                      "FP2"=c(0.2, 0.76), "FP1"=c(-0.2, 0.76), "AF2"=c(0.26, 0.62), 
+                      "AF1"=c(-0.26, 0.62), "AF8"=c(0.45, 0.68), "AF7"=c(-0.45, 0.68), 
+                      "F2"=c(0.21, 0.41), "F1"=c(-0.21, 0.41), "F4"=c(0.4, 0.45), 
+                      "F3"=c(-0.4, 0.45), "F6"=c(0.55, 0.5), "F5"=c(-0.55, 0.5), 
+                      "F8"=c(0.65, 0.55), "F7"=c(-0.65, 0.55), "FC2"=c(0.25, 0.21), 
+                      "FC1"=c(-0.25, 0.21), "FC4"=c(0.5, 0.22), "FC3"=c(-0.5, 0.22), 
+                      "FC6"=c(0.7, 0.26), "FC5"=c(-0.7, 0.26), "FT8"=c(0.9, 0.31), 
+                      "FT7"=c(-0.9, 0.31), "CP2"=c(0.25, -0.21), "CP4"=c(0.5, -0.22), 
+                      "CP6"=c(0.7, -0.26), "TP8"=c(0.9, -0.31), "CP1"=c(-0.25, -0.21), 
+                      "CP3"=c(-0.5, -0.22), "CP5"=c(-0.7, -0.26), "TP7"=c(-0.9, -0.31), 
+                      "P2"=c(0.21, -0.41), "P4"=c(0.4, -0.45), "P6"=c(0.55, -0.5), 
+                      "P8"=c(0.65, -0.55), "P1"=c(-0.21, -0.41), "P3"=c(-0.4, -0.45), 
+                      "P5"=c(-0.55, -0.5), "P7"=c(-0.65, -0.55), "PO2"=c(0.2, -0.62), 
+                      "PO8"=c(0.45, -0.68), "PO1"=c(-0.2, -0.62), "PO7"=c(-0.45, -0.68), 
+                      "O2"=c(0.2, -0.85), "O1"=c(-0.2, -0.85))
+layMat <- matrix(NA, nrow=64, ncol=2)
+for (i in 1:length(node.names)){
+  x <- node.names[i]
+  layMat[i, 1] <- unlist(position.List)[paste(x, 1, sep="")]
+  layMat[i, 2] <- unlist(position.List)[paste(x, 2, sep="")]
+}
+
+# Step 3. Prepare adjancecy matrix x network differenziale
+adj_group = G.our.symm.weighted$group
+adj_group = log10(adj_group)        # Logscale
+adj_group[is.na(adj_group)] = 0
+diag(adj_group) <- 0
+
+vals <- adj_group[adj_group != 0 & !is.na(adj_group)]
+greates_vals <- vals[abs(vals)>2]
+perc_greatest <- length(greates_vals)/length(vals)
+
+abs_thr <- quantile(abs(vals), probs =0.0001, na.rm = TRUE)
+adj_extreme <- adj_group
+adj_extreme[abs(adj_group) < abs_thr] <- 0
+
+# Step 4. Plot
+#pdf("diff_network_AUD.pdf", width = 11, height = 7)
+adj_plot = adj_extreme
+colnames(adj_plot) <- node.names
+row.names(adj_plot) <- node.names
+net <- graph_from_adjacency_matrix(adj_plot, mode = "undirected",
+            weighted = TRUE,diag = FALSE)
+w <- E(net)$weight
+max_range = max(abs(w))
+ncol <- 1000
+col_fun <- colorRampPalette(c("darkblue", "white", "darkred"))
+cols <- col_fun(ncol)
+E(net)$color <- cols[
+  as.numeric(cut(w, breaks = seq(-max_range, max_range, length.out = ncol+1),
+                 include.lowest = TRUE))
+]
+layout(matrix(c(1, 2, 3), nrow = 1), widths = c(4, 0.5, 0.5))
+par(mar = c(0, 0, 0, 0))
+ord <- order(abs(E(net)$weight))
+# Rebuild graph with reordered edges
+# net2 <- graph_from_data_frame(
+#   d = as_data_frame(net, what = "edges")[ord, ],
+#   directed = FALSE,
+#   vertices = as_data_frame(net, what = "vertices")
+# )
+
+net2 <- igraph::graph_from_data_frame(
+  d = igraph::as_data_frame(net, what = "edges")[ord, ],
+  directed = FALSE,
+  vertices = igraph::as_data_frame(net, what = "vertices")
+)
+#net <- net %>% igraph::permute.edges(ord)
+#alpha_vals <- scales::rescale(abs(E(net2)$weight), to = c(0.3, 1))
+E(net2)$color <- adjustcolor(E(net2)$color, alpha.f = 0.7)
+plot(net2, layout = layMat,
+     vertex.size = 3, edge.width=2, vertex.label.color = "black", vertex.label.font = 2,
+     vertex.label.dist=1, vertex.label.cex = 2, vertex.color = "orange", 
+     edge.curved = 0.2, margin =0)
+# ---- ColorbarS ----
+par(fig = c(0.8, 0.83, 0.2, 0.8), new = TRUE, mar = c(2,0,2,0))
+image(
+  z = t(matrix(seq(-max_range, max_range, length.out = ncol+1), ncol = 1)),
+  col = cols,
+  xaxt='n',
+  yaxt='n'
+)
+axis(4, at=seq(0,1,length.out=5), cex.axis = 2,
+     labels=round(seq(-max_range, max_range, length.out=5),2))
+
+mtext(
+  expression('Edge weight (log'[10]*' scale)'),
+  side = 4,
+  line = 4,
+  cex = 1.7
+)
+
+box()
+#dev.off()
+
+
